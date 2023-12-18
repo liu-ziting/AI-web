b3b522e5953f267d136e23ab9fdad71321b9fdec
liuziting <57311725+liu-ziting@users.noreply.github.com>
2023-12-18 16:09:26 +0800

    更新数据
    更新数据

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,12 @@
 // List of categories for blog posts
-export const CATEGORIES = ['AI编程', '聊天机器人', 'AI助手', 'AI图像', 'AI效率', 'AI声音', 'AI设计', '其他'] as const
+export const CATEGORIES = [
+    'AI编程',
+    '聊天机器人',
+    'AI助手',
+    'AI图像',
+    'AI效率',
+    'AI声音',
+    'AI设计',
+    'Prompt',
+    '其他'
+] as const
