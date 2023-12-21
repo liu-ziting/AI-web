bb9921d0ceb04c9975da4f9894077239fc0c49a5
liuziting <57311725+liu-ziting@users.noreply.github.com>
2023-12-21 18:27:09 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,5 +30,14 @@ export default defineConfig({
         }),
         sitemap(),
         tailwind()
-    ]
+    ],
+    scripts: {
+        postRender: `
+        (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "k56ohtr6tn");
+        `
+    }
 })
